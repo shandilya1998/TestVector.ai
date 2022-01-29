07d005e4633b094960685a5656ee4a8f588afb68
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2022-01-29 23:41:16 +0530

    commit pub-sub barebones code

---
+++ part2/constants.py
@@ -0,0 +1,7 @@
+params = {
+    'GOOGLE_APPLICATION_CREDENTIALS' : 'neuroengineering-334116-b41d7e598507.json',
+    'PUB_SUB_TOPIC'                  : 'python-tester',
+    'PUB_SUB_PROJECT'                : 'neuroengineering-334116',
+    'PUB_SUB_SUBSCRIPTION'           : 'python-tester-sub',
+    'timeout'                        : 3.0,
+}
